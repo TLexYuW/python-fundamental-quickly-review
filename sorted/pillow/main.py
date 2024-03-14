6fa870998dcc52048f6b2d21c88afddbfe75755e
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-03-15 00:15:00 +0800

    Update main.py

--- sorted/pillow/main.py
+++ sorted/pillow/main.py
@@ -6,13 +6,19 @@ def webp_to_gif(webp_file, gif_file):
     with Image.open(webp_file) as im:
         # Get the frame count and duration
         frame_count = im.n_frames
+        print(frame_count)
         frame_duration = im.info.get("duration", 50)
 
         # Create the GIF from the frames
         frames = []
         for frame in range(frame_count):
             im.seek(frame)
+            rgba_frame = im.convert("RGBA")
+            frame_data = rgba_frame.tobytes()
+            frame_bytes = len(frame_data)
+            print(f"Frame {frame}: {frame_bytes} bytes")
             frames.append(im.copy())
+        print(frames)
 
         # Save the frames as a GIF animation
         frames[0].save(
@@ -24,8 +30,31 @@ def webp_to_gif(webp_file, gif_file):
         )
 
 
-# Example usage
-webp_file = "./pillow/input.webp"
-gif_file = "./pillow/output.gif"
+def gif_to_webp(gif_file, webp_file):
+    with Image.open(gif_file) as im:
+        frame_count = im.n_frames
+        frame_duration = im.info.get("duration", 1000)
 
-webp_to_gif(webp_file, gif_file)
+        frames = []
+        for frame in range(frame_count):
+            im.seek(frame)
+            frames.append(im.copy())
+
+        frames[0].save(
+            webp_file,
+            save_all=True,
+            append_images=frames[1:],
+            duration=frame_duration,
+            optimize=True,
+            loop=0,
+            quality=10,
+        )
+
+
+if __name__ == "__main__":
+    # Example usage
+    webp_file = "./sorted/pillow/input2.webp"
+    gif_file = "./sorted/pillow/output.gif"
+
+    # webp_to_gif(webp_file, gif_file)
+    gif_to_webp(gif_file, webp_file)
